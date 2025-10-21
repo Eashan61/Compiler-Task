95b670ef9fd6b8e16ca3ef8b4ed6a11f0242b7c1
Reezwanul Haque Eashan <86619882+Eashan61@users.noreply.github.com>
2025-10-21 12:03:51 +0600

    Add files via upload

---
+++ task_1.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+
+using namespace std;
+
+int n1,n2,n3;
+
+char s1,s2;
+
+int main() {
+
+cout<<"Please enter your input!"<<endl;
+
+cin>>n1,n2,n3;
+cin>>s1,s1;
+
+if(cin>> n1,n2,n3){
+    cout<<"The input is Numaric constant!"<<endl;
+}else{
+    cout<<"The input is not numeric!"<<endl;
+}
+
+
+
+
+return 0;
+}
---
+++ task_2.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+
+using namespace std;
+
+char op = '+','-','*','/','%','=';
+
+string input;
+
+int main() {
+
+cout<<"Please enter your input!"<<endl;
+
+cin>>input;
+
+
+
+return 0;
+}
+
